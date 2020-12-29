cb28408c9a1f805bbaa4be6558886f32de194655
sohailshams <sohailshamas@yahoo.com>
2020-12-29 16:15:42 +0000

    Created BookList component to map over the book list

---
+++ src/components/Booklist.js
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react';
+import { BookContext } from '../context/BookContext';
+
+const BookList = () => {
+  const { books } = useContext(BookContext);
+  return books.length ? (
+    <div className="book-list">
+      <ul>
+        {books.map((book) => {
+          return <Bookdetails book={book} key={book.id} />;
+        })}
+      </ul>
+    </div>
+  ) : (
+    <div className="empty">No books to read!</div>
+  );
+};
+
+export default BookList;
