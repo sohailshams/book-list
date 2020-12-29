9022fa97b08ec7a886301b5c80cd9c8e6b66cce7
sohailshams <sohailshamas@yahoo.com>
2020-12-29 23:08:45 +0000

    Added bookReducer and updated respective functions to work accordingly

--- src/context/BookContext.js
+++ src/context/BookContext.js
@@ -1,23 +1,13 @@
-import React, { createContext, useState } from 'react';
-import { v1 as uuid } from 'uuid';
+import React, { createContext, useReducer } from 'react';
+import { bookReducer } from '../reducers/bookReducer';
 
 export const BookContext = createContext();
 
 export const BookContextProvider = (props) => {
-  const [books, setBooks] = useState([
-    { title: 'name of the wind', author: 'patrick rothfuss', id: 1 },
-    { title: 'booklist', author: 'sohail shams', id: 2 },
-  ]);
+  const [books, dispatch] = useReducer(bookReducer, []);
 
-  const addBook = (title, author) => {
-    setBooks([...books, { title, author, id: uuid() }]);
-  };
-
-  const removeBook = (id) => {
-    setBooks(books.filter((book) => book.id !== id));
-  };
   return (
-    <BookContext.Provider value={{ books, addBook, removeBook }}>
+    <BookContext.Provider value={{ books, dispatch }}>
       {props.children}
     </BookContext.Provider>
   );
